4eddac1dd1f2cde66a866e1d77a1b78c2a7f0329
cssmagic <cssmagic.cn@gmail.com>
2017-05-18 16:35:21 +0800

    Remove L2 implementation and polyfill implementation.

--- src/2.js
+++ src/2.js
@@ -1,55 +1,51 @@
-void function(){
+void function (WebPerf) {
 	'use strict'
 
-	//TODO: beacon
-
-	if (window.addEventListener) {
-		window.addEventListener('load', log, false)
-	} else if (window.attachEvent) {
-		window.attachEvent('onload', log)
-	} else {
-		var loadHandler = window.onload
-		window.onload = function(evt){
-			log() // loadHandler may throws, so call log first
-			if (typeof loadHandler === 'function') loadHandler.call(window, evt)
-		}
-	}
-
-	WebPerf.nt = function() {
+	// API
+	WebPerf.nt = function () {
 		return compact(WebPerf.navigationTiming())
 	}
-
-	WebPerf.setLogger = function(logger) {
+	WebPerf.setLogger = function (logger) {
 		this._logger = logger
 	}
 
+	// register when to log
 	function log() {
-		setTimeout(function(){
+		setTimeout(function () {
 			if (typeof WebPerf._logger === 'function') WebPerf._logger()
 		}, 1)
 	}
+	window.addEventListener('load', log, false)
+
 
 	var ignores = /^(name|entryType|startTime|duration|navigationStart)$/
-	function compact(t) {
-		//console.log(JSON.stringify(t))
-		var o = {T0: WebPerf.t0}
-		if (WebPerf.vendor) o.V = WebPerf.vendor
-		for (var k in t) {
-			if (ignores.test(k)) continue
-			if (!o[k]) {
-				var v = t[k]
-				switch (typeof v) {
-					case 'string': v = v.charAt(0)
-					case 'number': 
-						o[abbr(k)] = v
-				}
+
+	function compact(timing) {
+		var o = {}
+		// console.log(timing)
+		/** DEBUG_INFO_START **/
+		o.T0 = WebPerf.t0
+		/** DEBUG_INFO_END **/
+
+		for (var key in timing) {
+			if (ignores.test(key)) continue
+
+			var val = timing[key]
+			switch (typeof val) {
+				/** DEBUG_INFO_START **/
+				// handle `type`
+				case 'string':
+					val = val.charAt(0)
+				/** DEBUG_INFO_END **/
+				// fallthrough
+				case 'number':
+					o[abbr(key)] = val
 			}
 		}
-		//console.log(JSON.stringify(o))
+		// console.log(o)
 		return o
 	}
 
-	WebPerf.abbr = abbr
 	function abbr(name) {
 		var x
 		if (name.slice(-5) === 'Start') x = 0
@@ -57,15 +53,28 @@ void function(){
 		else x = ''
 
 		switch (name.split(/[A-Z]/, 1)[0]) {
-			case 'dom': return name.charAt(3) + x
-			case 'connect': return 'O' + x
-			case 'response': return 'P' + x
-			case 'request': return 'Q' + x
-			case 'secure': return 'S' + x
-			case 'link': return 'N' + x
-			case 'ms': return 'P' // msFirstPaint
-			default: return name.charAt(0).toUpperCase() + x
+			case 'dom':
+				return name.charAt(3) + x
+			case 'connect':
+				return 'O' + x
+			case 'response':
+				return 'P' + x
+			case 'request':
+				return 'Q' + x
+			case 'secure':
+				return 'S' + x
+			case 'link':
+				return 'N' + x
+			// msFirstPaint
+			case 'ms':
+				return 'P'
+			default:
+				return name.charAt(0).toUpperCase() + x
 		}
 	}
 
-}()
+	/** DEBUG_INFO_START **/
+	WebPerf.__abbr = abbr
+	/** DEBUG_INFO_END **/
+
+}(window.SWP)
